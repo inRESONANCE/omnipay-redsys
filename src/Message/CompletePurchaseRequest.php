4c9c1639a5ce06c45f0eb59ba526d17f990e6168
neverbot <neverbot@neverbot.com>
2015-11-24 20:28:10 +0100

    Log

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -40,7 +40,7 @@ class CompletePurchaseRequest extends PurchaseRequest
         $parameters = $query->get('Ds_MerchantParameters');
         $parameters = base64_decode(strtr($parameters, '-_', '+/'));
 
-        Log::info($parameters);
+        Illuminate\Log\Log::info($parameters);
 
         $data = array();
 
